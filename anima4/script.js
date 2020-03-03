2751a7fda45507758218ec76cdec49574fbbbfd5
millenacecilia <55113083+millenacecilia@users.noreply.github.com>
2020-03-02 20:25:52 -0500

    Add files via upload

---
+++ anima4/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    $(".message").addClass("alarm");
+    (".snooze").addClass("alarm");
+   	(".starss").addClass("alarm");
+    (".man").addClass("alarm");
+  }, 3000);
+});
+
