4d96515423264b7f5ecbe0ef91a2e74f13762f77
millenacecilia <55113083+millenacecilia@users.noreply.github.com>
2020-03-26 14:22:54 -0400

    Add files via upload

--- mugen-distribution-master 6/demo/script.js
+++ mugen-distribution-master 6/demo/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 3200, npcCollisionRadius: 100, actionDuration:600, jumpHeight: 300});
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 50, timeBetweenNPCs: 3000, npcCollisionRadius: 100, actionDuration:600, jumpHeight: 250, npcSpeed: 500, removeNPCAfterInteractedDelay: 5000, removeNPCAfterInteracted: true});
 });
