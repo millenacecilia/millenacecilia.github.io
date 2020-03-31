36e505424260e6c7b548497896cae3704954dbd9
millenacecilia <55113083+millenacecilia@users.noreply.github.com>
2020-03-31 19:01:48 -0400

    Add files via upload

---
+++ pizzarat/demo/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var mugen = new MUGEN();
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 50, timeBetweenNPCs: 3000, npcCollisionRadius: 100, actionDuration:350, jumpHeight: 250, npcSpeed: 500, removeNPCAfterInteractedDelay: 1000, removeNPCAfterInteracted: true});
+});
